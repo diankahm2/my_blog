733af7ee1d28fc2e5385b7a6bb052a95e1ec703b
diankahm2 <diana.omarova2013@gmail.com>
2019-01-28 19:44:24 +0600

    Created Django Project

---
+++ hello.py
@@ -0,0 +1,10 @@
+a=2
+b=3
+c=a+b
+print(c)
+print("Hello")
+list=[1,2,3,35]
+for i in list:
+	print(i)
+if (a>3):
+print("dfsfsd")
---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/diana/Desktop/django_2/myenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
